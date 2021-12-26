d4c7ccb2b18f668da48bab2ea4015757d8730126
Narek Manukyan <gendrarium@yandex.ru>
2021-12-27 00:43:08 +0300

    fix: change settings for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+
 import App from './components/App/App';
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.querySelector('.root'),
 );
